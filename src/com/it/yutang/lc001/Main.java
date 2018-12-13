c0f52b1f1ed7ee91d2eb68f50b9707eee673984a
YuTang <It.YuTang@gmail.com>
2018-12-13 16:50:20 +0800

    format

--- src/com/it/yutang/lc001/Main.java
+++ src/com/it/yutang/lc001/Main.java
@@ -12,16 +12,17 @@ import java.util.Map;
 class Main {
     public static void main(String[] args) {
 
-        int[] array = new int[]{2,7,11,15};
+        int[] array = new int[] { 2, 7, 11, 15 };
         int target = 9;
 
-        System.out.println(Arrays.toString(twoSum01(array,target)));
-        System.out.println(Arrays.toString(twoSum02(array,target)));
+        System.out.println(Arrays.toString(twoSum01(array, target)));
+        System.out.println(Arrays.toString(twoSum02(array, target)));
 
     }
 
     /**
      * 两层循环遍历
+     *
      * @param nums
      * @param target
      * @return
@@ -29,8 +30,8 @@ class Main {
     public static int[] twoSum01(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target){
-                    return new int[]{i,j};
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
@@ -40,19 +41,20 @@ class Main {
 
     /**
      * 哈希表一次遍历
+     *
      * @param nums
      * @param target
      * @return
      */
     public static int[] twoSum02(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
             int result = target - nums[i];
-            if (map.containsKey(result)){
-                return new int[]{map.get(result),i};
+            if (map.containsKey(result)) {
+                return new int[] { map.get(result), i };
             }
 
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
 
         throw new IllegalArgumentException("Solution Not Found");
