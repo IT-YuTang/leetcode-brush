aecd7ec0fc75902bd2b2ad58da425214c51c030c
YuTang <It.YuTang@gmail.com>
2018-12-14 15:37:32 +0800

    comments

--- src/com/it/yutang/lc002/Main.java
+++ src/com/it/yutang/lc002/Main.java
@@ -13,17 +13,21 @@ public class Main {
     }
 
     public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
-        ListNode header = new ListNode(0);
+        ListNode header = new ListNode(0);//表头
         ListNode curNode = header;
 
+        //进位
         int carry = 0;
         while (node1 != null || node2 != null) {
             int node1Val = node1 == null ? 0 : node1.val;
             int node2Val = node2 == null ? 0 : node2.val;
 
+            //当前节点值
             int sum = carry + node1Val + node2Val;
+            //是否进位
             carry = sum / 10;
 
+            //新节点一位表示
             curNode.next = new ListNode(sum % 10);
             curNode = curNode.next;
 
